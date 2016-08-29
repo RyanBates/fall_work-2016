078970b5e6d271f7e14a9f8b03e4561070e1a1d6
Ryan Bates <ryan.bates@students.aie.edu.au>
2016-08-29 17:24:02 -0500

    starting over from mistakes

--- morning practices/8-29-16/projection_matrices/Source.cpp
+++ morning practices/8-29-16/projection_matrices/Source.cpp
@@ -6,33 +6,9 @@ and what are there positions relative to the matrix
 hint: 2 / right - left is x/*/
 #include <iostream>
 
-float boxvertices[] = {
-	2,0,0,0,
-	0,2,0,0,
-	0,0,-2,0,
-	0,0,0,1 };
-
-float left[] = { -2,0,0,0 };
-float right[] = { 2,0,0,0 };
-
-int x =  2/ right[0] - left[0];
-
-void translate()
-{
-	float matrix = boxvertices[15] - x;
-	boxvertices - x;
-	std::cout << boxvertices[00] << " " << boxvertices[01] << " " << boxvertices[02] << " " << boxvertices[03] << std::endl;
-	std::cout << boxvertices[04] << " " << boxvertices[05] << " " << boxvertices[06] << " " << boxvertices[07] << std::endl;
-	std::cout << boxvertices[8] << " " << boxvertices[9] << " " << boxvertices[10] << " " << boxvertices[11] << std::endl;
-	std::cout << boxvertices[12] << " " << boxvertices[13] << " " << boxvertices[14] << " " << boxvertices[15] << std::endl;
-	std::cout << std::endl;
-	std::cout << matrix << std::endl;
-
-}
 
 int main()
 {
-	translate();
 	system("pause");
 	return 0;
 }
