31da1f8b9456e67ba51dc860d09528b445b58cda
Ryan Bates <ryan.bates@students.aie.edu.au>
2016-08-30 09:21:06 -0500

    just showing what i have right now

---
+++ morning practices/8-22-16/8-22-16/Source.cpp
@@ -0,0 +1,26 @@
+//write a c++ program that creates the identity matrix.
+#include <iostream>
+
+int rows = 4;
+float colomns = 4;
+float matrix[01];
+
+void identity()
+{
+	for (float  i = rows; i < rows; i++)
+	{
+
+		for (float j = colomns; j < colomns; j++)
+		{
+			
+		}
+	}
+}
+
+
+int main()
+{
+	identity();
+	system("pause");
+	return 0;
+}
---
+++ morning practices/8-23-16/8-23-16/Source.cpp
@@ -0,0 +1,12 @@
+/*/write a program that generates a matrix translation for any identity matrix
+for example
+what is the resulting matrix when transformation of[5, 3, 2, 1] is applied to the identity
+this should work for any arbitrary matrix/*/
+#include <iostream>
+
+int main()
+{
+
+	system("puase");
+	return 0;
+}
