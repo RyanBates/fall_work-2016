7d1e60911567ec3c444360f51c39a821a0e36b1b
Ryan Bates <ryan.bates@students.aie.edu.au>
2016-08-31 11:00:33 -0500

    changes on prime have it to where it prints every num from 1 to 99

--- morning practices/8-31-16/8-31-16/Source.cpp
+++ morning practices/8-31-16/8-31-16/Source.cpp
@@ -3,42 +3,38 @@ write a function that uses that function to find all prime numbers under 100.
 write a function that determines if a number is a multiple of another number.
 use your functions together to find all prime numbers below 1000.*/
 #include <iostream>
-int primenum = 1;
+int primenum;
 int AON; // any other number
 bool Prime = false;
-bool findallprimenums = false;
 
-void prime() //a prime number is a number that is only divisable by itself or 1;
-{
-	if (primenum / 1 == 1 || primenum / primenum == 1)
-	{
-		Prime = true;
-	}
-
-	if (primenum / AON == AON)
-	{
-		Prime = false;
-	}
-
-	if (AON != 1)
-	{
-		Prime = false;
-	}
-}
-
-
-void prime100()
+void prime100() //a prime number is a number that is only divisable by itself or 1;
 {
 	for (int i = 0; i < 100; i++)
 	{
-		primenum = i;
-		while (primenum == i)
+		for (int j = 0; j < 100; j++)
 		{
-			Prime = true;
-			if (primenum = i % (i));
+			AON = j;
+			if (primenum = i % (100));
 			{
-				findallprimenums = true;
-				std::cout << primenum << std::endl;
+				if (primenum / 1 == 1 || primenum / primenum == 1)
+					Prime = true;
+
+				if (AON != 1)
+					Prime = false;
+
+				while (AON != 1 || AON != primenum)
+				{
+					if (primenum / AON == AON)
+					{
+						Prime = false;
+					}
+				}
+
+				if (Prime = true)
+					std::cout << primenum << std::endl;
+
+				else
+					std::cout << primenum << std::endl;
 			}
 		}
 	}
@@ -46,7 +42,7 @@ void prime100()
 
 int main()
 {
-	std::cout << primenum << std::endl;
+	prime100();
 	system("pause");
 	return 0;
 }
