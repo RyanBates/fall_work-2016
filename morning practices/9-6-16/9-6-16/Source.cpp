d28f0f82a663ac381f41b82cf9db69f21061f7ba
Ryan Bates <ryan.bates@students.aie.edu.au>
2016-09-06 11:15:15 -0500

    adding todays morning assignment

---
+++ morning practices/9-6-16/9-6-16/Source.cpp
@@ -0,0 +1,35 @@
+/*
+write a function that takes a function as a parameter. The function it receives should take two arguments (min,max).
+Calculate a random number between those two numbers and return it. If the function fails return or assert on the call.
+if the function that takes the min,max function fails give feedback to the user.
+*/
+#include <iostream>
+
+float num;
+float random;
+float give(float min, float max) 
+{
+	min = 1;
+	max = 100;
+	return give(1, 100);
+}
+
+float take(give(1,100))
+{
+	if (random == 0)
+	{
+		if (min < max)
+		{
+			num = max % min;
+			return num;
+			std::cout << num << std::endl;
+		}
+	}
+}
+
+
+void main()
+{
+	take();
+	system("pause");
+}
